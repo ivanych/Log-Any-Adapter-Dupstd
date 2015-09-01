d883a9dc153a3b3a5bd888cfaa99f43536637351
Mikhail Ivanov <m.ivanov@i-free.com>
2015-09-01 18:09:12 +0300

    Created skeleton module

---
+++ lib/Log/Any/Adapter/Dupstd.pm
@@ -0,0 +1,107 @@
+package Log::Any::Adapter::Dupstd;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Log::Any::Adapter::Dupstd - The great new Log::Any::Adapter::Dupstd!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Log::Any::Adapter::Dupstd;
+
+    my $foo = Log::Any::Adapter::Dupstd->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+MIkhail Ivanov, C<< <m.ivanych at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-log-any-adapter-dupstd at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Log-Any-Adapter-Dupstd>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Log::Any::Adapter::Dupstd
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Log-Any-Adapter-Dupstd>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Log-Any-Adapter-Dupstd>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Log-Any-Adapter-Dupstd>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Log-Any-Adapter-Dupstd/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 MIkhail Ivanov.
+
+This program is released under the following license: perl_5
+
+
+=cut
+
+1; # End of Log::Any::Adapter::Dupstd
